cd1ce727fd1b2ef2eb64e48911c0d7be0604d49e
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2017-12-10 10:24:15 +0100

    Up2

--- src/main/java/eu/jedlik/OkjKiralynokJava/App.java
+++ src/main/java/eu/jedlik/OkjKiralynokJava/App.java
@@ -2,7 +2,7 @@ package eu.jedlik.OkjKiralynokJava;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
        new Kiralynok(); // 0. feladat
     }
