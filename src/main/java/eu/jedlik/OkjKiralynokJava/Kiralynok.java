50acb5da5a97c686b490d138a80a25e77efd244d
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2017-12-10 10:28:55 +0100

    Up4

--- src/main/java/eu/jedlik/OkjKiralynokJava/Kiralynok.java
+++ src/main/java/eu/jedlik/OkjKiralynokJava/Kiralynok.java
@@ -3,5 +3,8 @@ package eu.jedlik.OkjKiralynokJava;
 public class Kiralynok // 0. feladat
 {
     public Kiralynok() {
+        System.out.println("4. feladat: Az üres tábla:");
+        Tábla tábla = new Tábla('#');
+        tábla.Megjelenít();
     }   
 }
--- src/main/java/eu/jedlik/OkjKiralynokJava/Tábla.java
+++ src/main/java/eu/jedlik/OkjKiralynokJava/Tábla.java
@@ -15,6 +15,18 @@ public class Tábla // 1. feladat
                 T[i][j] = ÜresCella; // 3.c
             }
         }
-
     }
+
+    public void Megjelenít() // 4. feladat
+    {
+        for (int sor = 0; sor < 8; sor++)
+        {
+            for (int oszlop = 0; oszlop < 8; oszlop++)
+            {
+                System.out.print(T[sor][oszlop]);
+            }
+            System.out.println();
+        }
+    }
+
 }
