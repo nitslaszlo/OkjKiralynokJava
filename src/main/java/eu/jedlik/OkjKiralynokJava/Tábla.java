262fad8540d31ea90bf5dbda2906eb1f08352658
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2017-12-10 10:20:23 +0100

    Up3

--- src/main/java/eu/jedlik/OkjKiralynokJava/Tábla.java
+++ src/main/java/eu/jedlik/OkjKiralynokJava/Tábla.java
@@ -4,4 +4,17 @@ public class Tábla // 1. feladat
 {
     private char[][] T; // 2. feladat
     private char ÜresCella; // 2. feladat
+
+    public Tábla(char üresCella){ // 3. feladat
+        T = new char[8][8]; // 3.a
+        ÜresCella = üresCella; // 3.b
+        for (int i = 0; i < 8; i++) // 3.c
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                T[i][j] = ÜresCella; // 3.c
+            }
+        }
+
+    }
 }
