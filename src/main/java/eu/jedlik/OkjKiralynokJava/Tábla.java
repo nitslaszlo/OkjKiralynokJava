b478f2f7bfff2dff08395e1d7e63f2063f2a9f1c
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2017-12-12 08:33:36 +0100

    Up5

--- src/main/java/eu/jedlik/OkjKiralynokJava/Tábla.java
+++ src/main/java/eu/jedlik/OkjKiralynokJava/Tábla.java
@@ -9,13 +9,12 @@ public class Tábla // 1. feladat
 
     static private Random R = new Random();
 
-    public Tábla(char üresCella){ // 3. feladat
+    public Tábla(char üresCella) { // 3. feladat
         T = new char[8][8]; // 3.a
         ÜresCella = üresCella; // 3.b
         for (int i = 0; i < 8; i++) // 3.c
         {
-            for (int j = 0; j < 8; j++)
-            {
+            for (int j = 0; j < 8; j++) {
                 T[i][j] = ÜresCella; // 3.c
             }
         }
@@ -23,10 +22,8 @@ public class Tábla // 1. feladat
 
     public void Megjelenít() // 4. feladat
     {
-        for (int sor = 0; sor < 8; sor++)
-        {
-            for (int oszlop = 0; oszlop < 8; oszlop++)
-            {
+        for (int sor = 0; sor < 8; sor++) {
+            for (int oszlop = 0; oszlop < 8; oszlop++) {
                 System.out.print(T[sor][oszlop]);
             }
             System.out.println();
@@ -35,17 +32,14 @@ public class Tábla // 1. feladat
 
     public void Elhelyez(int n) // 5. feladat
     {
-        for (int i = 0; i < n;)
-        {
+        for (int i = 0; i < n;) {
             int sor = R.nextInt(8);
             int oszlop = R.nextInt(8);
-            if (T[sor][oszlop] == ÜresCella)
-            {
+            if (T[sor][oszlop] == ÜresCella) {
                 T[sor][oszlop] = 'K';
                 i++;
             }
         }
     }
-
 
 }
